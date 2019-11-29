bf1a45b8977fb34fa22073b33a9a1275c3cc24d5
jackychencw@gmail.com <jackychencw@gmail.com>
2019-11-29 17:44:36 -0500

    added six, updated readme

--- README.m
+++ README.m
@@ -5,3 +5,6 @@ sudo pip install -r requirements.txt
 If there is error saying package SIX cannot be uninstalled, use following command instead:
 sudo pip install --ignore-installed six -r requirements.txt
 
+Since I had problem with package six version, therefore I have included six-1.13.0. cd into this folder and run sudo python setup.py install should install the newest version of six, then you can rerun command above, should work.
+
+
